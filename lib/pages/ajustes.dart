59f986d8aa7d041f86d8bef8f5b90a3260b9ede4
julissag0319 <julissagabriela_97@yahoo.com>
2021-11-26 12:30:55 -0600

    segundo cambio

--- lib/pages/ajustes.dart
+++ lib/pages/ajustes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AjustePages extends StatefulWidget {
   const AjustePages({Key? key}) : super(key: key);
@@ -8,11 +9,41 @@ class AjustePages extends StatefulWidget {
 }
 
 class _AjustePagesState extends State<AjustePages> {
+  final pin = TextEditingController();
+  String pinguardado = "";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cargarpin();
+  }
+
+  void cargarpin() async {
+    final pref = await SharedPreferences.getInstance();
+    pin.text = pref.getString("pin") ?? "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Ajustes"),
+      ),
+      body: Column(
+        children: [
+          Text(pin.text),
+          TextField(
+            decoration: InputDecoration(hintText: "Ingrese su Pin."),
+            controller: pin,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final pref = await SharedPreferences.getInstance();
+          pref.setString("pin", pin.text);
+        },
+        child: Icon(Icons.save),
       ),
     );
   }
